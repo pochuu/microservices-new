3d23f97367094de3336672654ff73aada0f67c41
pochu <jonasz.kasprzak@gmail.com>
2023-02-03 10:17:53 +0100

    eureka server configuration

--- inventory-service/src/main/java/com/pochu/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/pochu/inventoryservice/repository/InventoryRepository.java
@@ -2,10 +2,8 @@ package com.pochu.inventoryservice.repository;
 
 import com.pochu.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
